148a463f5f1e7e5798b9e65130782bea05c54160
Nazeef Nuwaisir Khan <nazeef05khan@gmail.com>
2025-11-17 06:32:06 +0800

    Fix main method signature to accept arguments

--- Viva1/V01Q02.java
+++ Viva1/V01Q02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class V01Q02 {
-   public static void main() {
+   public static void main(String [] args) {
        Scanner input = new Scanner(System.in);
 
        int q = input.nextInt();
